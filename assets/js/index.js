9acd20037d20bf97e29a84dfab19035a4119b279
Justin Duong <jduong@umich.edu>
2023-04-27 13:01:04 -0400

    moved js file to own folder. Finished css for project. Cleared form when form is validated and submit button is clicked.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     // index 0 - 4 are elements that need to be validated. 5th/last one is submit button.
     for (let i = 0; i < 5; i++) {
-      if (!formElements[i].validity.valid) {
+      if (!checkValidity(formElements[i])) {
         return false;
       }
     }
     // form is valid, run logic for signing up user...
+    // Signing up user, let's just clear the input's values.
+    clearForm();
   });
 
   // Event Listener for each input, checking validity on input.
@@ -33,19 +35,21 @@ const checkValidity = (input) => {
   if (input.id === 'zipcode') {
     const regex = new RegExp('^[0-9]{5}(?:-[0-9]{4})?$');
     if (!regex.test(input.value)) {
+      input.classList.add('error');
       spanChildElement.innerHTML = 'Error: Input does not match pattern: xxxxx or xxxxx-xxxx';
       spanChildElement.classList.add('active');
+      return false;
     } else {
+      input.classList.remove('error');
       spanChildElement.innerHTML = '';
       spanChildElement.classList.remove('active');
     }
-    return;
+    return true;
   }
 
   // Separate check for password input.
   if (input.id === 'password') {
-    checkPasswordValidity();
-    return;
+    return checkPasswordValidity();
   }
 
   // Separate check if password confirmation matches password.
@@ -53,41 +57,54 @@ const checkValidity = (input) => {
     const password = document.getElementById('password').value;
 
     if (input.value !== password) {
+      input.classList.add('error');
       spanChildElement.innerHTML = 'Error: Passwords do not match.';
       spanChildElement.classList.add('active');
+      return false;
     } else {
+      input.classList.remove('error');
       spanChildElement.innerHTML = '';
       spanChildElement.classList.remove('active');
     }
-    return;
+    return true;
   }
 
   // Input that is required and is missing.
   if (input.validity.valueMissing) {
+    input.classList.add('error');
     spanChildElement.innerHTML = 'Error: Input is missing.';
     spanChildElement.classList.add('active');
+    return false;
   } else if (input.validity.typeMismatch) {
     // Input that does not match email format.
+    input.classList.add('error');
     spanChildElement.innerHTML = 'Error: Invalid email format ex: xxx@email.com';
     spanChildElement.classList.add('active');
+    return false;
   } else if (input.validity.tooLong) {
     // Input that is longer than maxLength attribute.
+    input.classList.add('error');
     spanChildElement.innerHTML = `Error: Input is ${input.value.length} characters. Should be ${input.max} characters.`;
     spanChildElement.classList.add('active');
+    return false;
   } else {
     // Valid input, remove error message.
+    input.classList.remove('error');
     spanChildElement.innerHTML = '';
     spanChildElement.classList.remove('active');
   }
+  return true;
 };
 
 
 const checkPasswordValidity = () => {
+  const input = document.getElementById('password');
   const spanPasswordError = document.getElementById('error-password');
   const divPasswordRequirement = document.getElementById('password-requirements');
 
   const password = document.getElementById('password').value;
   if (password.length < 8) {
+    input.classList.add('error');
     spanPasswordError.innerHTML = 'Error: Password not longer than 8 characters';
     spanPasswordError.classList.add('active');
     divPasswordRequirement.classList.remove('hide');
@@ -99,14 +116,25 @@ const checkPasswordValidity = () => {
   const hasNumbers = /\d/.test(password);
   const hasNonalphas = /\W/.test(password);
   if (hasUpperCase + hasLowerCase + hasNumbers + hasNonalphas < 3) {
+    input.classList.add('error');
     spanPasswordError.innerHTML = 'Error: Password does not meet requirements.';
     spanPasswordError.classList.add('active');
     divPasswordRequirement.classList.remove('hide');
     return false;
   }
 
+  input.classList.remove('error');
   spanPasswordError.innerHTML = '';
   spanPasswordError.classList.remove('active');
   divPasswordRequirement.classList.add('hide');
   return true;
 };
+
+const clearForm = () => {
+  const form = document.getElementById('form-create-user');
+  const elementsForms = form.elements;
+
+  for (let i = 0; i < 5; i++) {
+    elementsForms[i].value = '';
+  }
+}
